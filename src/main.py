9419d0f8a9ddac532c40ec727a5fa0c8e287aafb
manolo <manoloesparta@gmail.com>
2019-01-11 17:04:19 -0800

    update

--- src/main.py
+++ src/main.py
@@ -16,10 +16,13 @@ def main():
 	removeSongs(Spotify, username, playlist_goal)
 
 	albums = getAlbums(Spotify, username, playlist_retrieved)
-	albums = selectAlbums(albums['albums_uri'], albums['albums_name'],albums['albums_artist'])
-	tracks = selectSongs(Spotify, albums['albums_selected'], albums['albums_selected_name'], albums['albums_selected_artist'])
+	albums = selectAlbums(albums['albums_uri'],
+	                      albums['albums_name'], albums['albums_artist'])
+	tracks = selectSongs(Spotify, albums['albums_selected'],
+	                     albums['albums_selected_name'], albums['albums_selected_artist'])
 
 	Spotify.user_playlist_add_tracks(username, playlist_goal, tracks['tracks_id'])
+
 
 if __name__ == '__main__':
 	main()
