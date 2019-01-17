caab0a6fa734d512c33e80d705cdd6b5b0ccb1dc
Manuel Esparza <manoloesparta@gmail.com>
2019-01-17 00:04:08 -0800

    [update main.py] try/except block for env variables

--- src/main.py
+++ src/main.py
@@ -4,11 +4,16 @@ from playlist.source import Source
 from playlist.goal import Goal
 
 def main():
-    username = ev['USERNAME']
-    client_id, client_secret = ev['CLIENT_ID'], ev['CLIENT_SECRET']
-    spotify_user = SpotifyUser(username, client_id, client_secret)
 
-    playlist_src, playlist_goal = ev['PLAYLIST_SRC'], ev['PLAYLIST_GOAL']
+    try:
+        username = ev['USERNAME']
+        client_id, client_secret = ev['CLIENT_ID'], ev['CLIENT_SECRET']
+        spotify_user = SpotifyUser(username, client_id, client_secret)
+        playlist_src, playlist_goal = ev['PLAYLIST_SRC'], ev['PLAYLIST_GOAL']
+
+    except KeyError as k:
+        print(k, 'must be defined in the environment or you need to define the variables with your custom data')
+
     source = Source(playlist_src, spotify_user)
     goal = Goal(playlist_goal, spotify_user)
 
